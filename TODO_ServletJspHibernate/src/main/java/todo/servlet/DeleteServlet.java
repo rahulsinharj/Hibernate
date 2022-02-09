e9b0e628fb2bfc3f2959f87f90531a82b94cf31d
rahul <rahul@192.168.1.3>
2022-02-10 02:25:12 +0530

    TODO_servletJsp

---
+++ TODO_ServletJspHibernate/src/main/java/todo/servlet/DeleteServlet.java
@@ -0,0 +1,41 @@
+package todo.servlet;
+
+import java.io.IOException;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import todo.entities.Note;
+import todo.helper.FactoryProvider;
+
+public class DeleteServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public DeleteServlet() {
+		super();
+
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		try {
+			int noteId = Integer.parseInt(request.getParameter("note_id").trim());
+
+			Session s = FactoryProvider.getFactory().openSession();
+			Transaction tx = s.beginTransaction();
+			Note note = (Note) s.get(Note.class, noteId);
+			s.delete(note);
+			tx.commit();
+			s.close();
+			response.sendRedirect("all_notes.jsp");
+
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+
+}
---
+++ TODO_ServletJspHibernate/src/main/java/todo/servlet/SaveNoteServlet.java
@@ -0,0 +1,52 @@
+package todo.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import todo.entities.Note;
+import todo.helper.FactoryProvider;
+
+/**
+ * Servlet implementation class SaveNoteServlet
+ */
+public class SaveNoteServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public SaveNoteServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		try {
+			// title,content fetch
+			String title = request.getParameter("title");
+			String content = request.getParameter("content");
+			Note note = new Note(title, content, new Date());
+//			System.out.println(note.getId() + " : " + note.getTitle());
+			// hibernate:save()
+			Session s = FactoryProvider.getFactory().openSession();
+			Transaction tx = s.beginTransaction();
+			s.save(note);
+			tx.commit();
+			s.close();
+			response.setContentType("text/html");
+			PrintWriter out = response.getWriter();
+			out.println("<h1 style='text-align:center;'>Note is added successfully</h1>");
+			out.println("<h1 style='text-align:center;'><a href='all_notes.jsp'>View all notes</a></h1>");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
