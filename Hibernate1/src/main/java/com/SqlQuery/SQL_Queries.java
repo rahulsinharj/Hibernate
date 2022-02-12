eae9046831d53f99033ba7f911f54f00376992a1
rahul <rahul@192.168.1.6>
2022-02-12 12:14:00 +0530

    SQL_Queries

--- Hibernate1/src/main/java/com/SqlQuery/SQL_Queries.java
+++ Hibernate1/src/main/java/com/SqlQuery/SQL_Queries.java
@@ -1,8 +1,11 @@
 package com.SqlQuery;
 
+import java.util.List;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.query.NativeQuery;
 
 public class SQL_Queries {
 
@@ -12,11 +15,19 @@ public class SQL_Queries {
 		Session ses = factory.openSession(); // factory.getCurrentSession();
 		
 		
-/*		// SELECTING nonDynamic entries through HQL Syntax::
- 		-------------------------------------------------									*/
+/*		// SELECTING entries through HQL Syntax::
+ 		-----------------------------------------											*/
+//		String query1 = "select * from student where course = 'Java Programming'";
+		String query1 = "select * from student where course = :x";							// Putting dynamic entries through :x
 		
+		NativeQuery nq = ses.createSQLQuery(query1); 
+		nq.setParameter("x", "Java Programming");
+		List<Object[]> sList = nq.list();
 		
-		
+		for(Object[] stu : sList) {
+//			System.out.println(Arrays.toString(stu));						// Showing all index values of returned Student Array 
+			System.out.println(stu[0] +" -- "+stu[4] +" -- "+stu[3]);		// Showing only some specific index values of returned Student Array 
+		}
 		
 		ses.close();
 		factory.close();
