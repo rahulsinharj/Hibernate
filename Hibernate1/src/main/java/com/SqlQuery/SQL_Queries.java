2b9ffe8c2b53760b6d7de62efbdab2b390b96e45
rahul <rahul@192.168.1.6>
2022-02-14 00:53:25 +0530

    NativeQuery

--- Hibernate1/src/main/java/com/SqlQuery/SQL_Queries.java
+++ Hibernate1/src/main/java/com/SqlQuery/SQL_Queries.java
@@ -7,6 +7,8 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.query.NativeQuery;
 
+import com.hql.Food;
+
 public class SQL_Queries {
 
 	public static void main(String[] args) {
@@ -15,23 +17,38 @@ public class SQL_Queries {
 		Session ses = factory.openSession(); // factory.getCurrentSession();
 		
 		
-/*		// SELECTING entries through HQL Syntax::
- 		-----------------------------------------											*/
+/*		// SELECTING STUDENT entries through SQL Query Syntax:: {without receiving into a proper class entity}
+ 		-------------------------------------------------------								*/
 //		String query1 = "select * from student where course = 'Java Programming'";
 		String query1 = "select * from student where course = :x";							// Putting dynamic entries through :x
 		
-		NativeQuery nq = ses.createSQLQuery(query1); 
-		nq.setParameter("x", "Java Programming");
-		List<Object[]> sList = nq.list();
+		NativeQuery nQuery = ses.createSQLQuery(query1); 
+		nQuery.setParameter("x", "Java Programming");
+		
+		List<Object[]> sList = nQuery.list();
 		
 		for(Object[] stu : sList) {
 //			System.out.println(Arrays.toString(stu));						// Showing all index values of returned Student Array 
 			System.out.println(stu[0] +" -- "+stu[4] +" -- "+stu[3]);		// Showing only some specific index values of returned Student Array 
 		}
+
+		
+/*		// SELECTING FOOD entries through SQL Query Syntax:: {receiving into a FOOD class entity}
+ 		----------------------------------------------------								*/
+//		NativeQuery sqlQuery = ses.createSQLQuery("select fid,foodName,food_rate from Food_Details"); // put names of all columns of RowEntries
+		NativeQuery sqlQuery = ses.createSQLQuery("select * from Food_Details");
+		sqlQuery.addEntity(Food.class);						// With this we are telling hibernate that we have to receive RowEntries{containing all ENTIRE Columns} in the form of a FOOD object.
+		
+		List<Food> foodList = sqlQuery.list(); 
+		
+		for(Food f : foodList) {
+			System.out.println(f);
+		}
+		
 		
 		ses.close();
 		factory.close();
-
+		
 	}
 
 }
