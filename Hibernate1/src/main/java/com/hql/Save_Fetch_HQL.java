5bdabab696a63679148ef223f1a2766b4ad050df
rahul <rahul@192.168.1.6>
2022-02-13 22:18:24 +0530

    Object[] arrays

--- Hibernate1/src/main/java/com/hql/Save_Fetch_HQL.java
+++ Hibernate1/src/main/java/com/hql/Save_Fetch_HQL.java
@@ -1,5 +1,6 @@
 package com.hql;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.hibernate.Session;
@@ -17,18 +18,20 @@ public class Save_Fetch_HQL {
 
 /*		// INSERTING Dynamic entries ::
  		--------------------------------------------------									*/
-		Transaction tx = ses.beginTransaction();
-		Food f1 = new Food(101,"Samosa",50);		ses.save(f1);
-		Food f2 = new Food(102,"Jalebi",50);		ses.save(f2);	
-		Food f3 = new Food(103,"Pepsi",20);			ses.save(f3);
-		Food f4 = new Food(104,"Coke",35);			ses.save(f4);
-		Food f5 = new Food(105,"Paneer",50);		ses.save(f5);
-		Food f6 = new Food(106,"Pizza",20);			ses.save(f6);
+//		Transaction tx = ses.beginTransaction();
+//		Food f1 = new Food(101,"Samosa",50);		ses.save(f1);
+//		Food f2 = new Food(102,"Jalebi",50);		ses.save(f2);	
+//		Food f3 = new Food(103,"Pepsi",20);			ses.save(f3);
+//		Food f4 = new Food(104,"Coke",35);			ses.save(f4);
+//		Food f5 = new Food(105,"Paneer",50);		ses.save(f5);
+//		Food f6 = new Food(106,"Pizza",20);			ses.save(f6);
+//		
+//		tx.commit();
 		
-		tx.commit();
 		
-/*		// SELECTING Dynamic entries through HQL Syntax ::
+/*		// FETCHING Dynamic entries through HQL Syntax ::
  		--------------------------------------------------									*/
+//		Query q1 = ses.createQuery("from Food");					// Without where clause , Query LIST will return all the columns -> for every RowEntries.
 		Query q1 = ses.createQuery("from Food where rate = :r");	// "Food" is the EntityName ; Also ByDefault EntityName is the Class name ; untill we specifically change it by doing  @Entity(name = "Food_Details") in Food class, in that case put "Food_Details"   
 		q1.setParameter("r", 50);									// But when we only change  @Table(name = "School_Details") ,  and no change in @Entity name, then in that case again our class name will be Entity Name, that we have to put here ClassName in HQL query 
 																	// And, after where condition for putting the where parameter -> either here we can write varible name "rate" or, Entity's Column changed name {@Column(name = "food_rate")} of Food class.
@@ -37,6 +40,17 @@ public class Save_Fetch_HQL {
 			System.out.println(f.getFid() + " -- " + f.getFoodName() + " -- " + f.getRate());
 		}
 
+		
+/*		// FETCHING some specific columns from table ::
+ 		--------------------------------------------------									*/
+		Query q2 = ses.createQuery("select fid,foodName,rate from Food");		// It will not return LIST of Food objects, now it will return LIST of Object[] arrays.
+		List<Object[]> fList = q2.list();
+		
+		for(Object[] fd : fList) {
+			System.out.println(Arrays.toString(fd));
+//			System.out.println(fd[0] +" -- "+fd[1] +" -- "+fd[2]);		// Showing only some specific index values of returned Food Array 
+		}
+		
 		
 		
 		ses.close();
