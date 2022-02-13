39fdf1e0a6f84adb66e2998619e889aec0df1724
rahul <rahul@192.168.1.6>
2022-02-13 21:42:41 +0530

    Fetch_HQL

--- Hibernate1/src/main/java/com/hql/Save_Fetch_HQL.java
+++ Hibernate1/src/main/java/com/hql/Save_Fetch_HQL.java
@@ -22,17 +22,16 @@ public class Save_Fetch_HQL {
 		Food f2 = new Food(102,"Jalebi",50);		ses.save(f2);	
 		Food f3 = new Food(103,"Pepsi",20);			ses.save(f3);
 		Food f4 = new Food(104,"Coke",35);			ses.save(f4);
-		Food f5 = new Food(105,"Paneer",50);		ses.save(f5);	
+		Food f5 = new Food(105,"Paneer",50);		ses.save(f5);
+		Food f6 = new Food(106,"Pizza",20);			ses.save(f6);
 		
 		tx.commit();
 		
 /*		// SELECTING Dynamic entries through HQL Syntax ::
  		--------------------------------------------------									*/
-		String query = "from Food where rate = 50";	// "Food" is the EntityName ; Also ByDefault EntityName is the Class name ; untill we specifically change it by doing  @Entity(name = "Food_Details") in Food class, in that case put "Food_Details"   
-															// But when we only change  @Table(name = "School_Details") ,  and no change in @Entity name, then in that case again our class name will be Entity Name, that we have to put here ClassName in HQL query 
-															// And, after where condition for putting the where parameter -> either here we can write varible name "rate" or, Entity's Column changed name {@Column(name = "food_rate")} of Food class.
-		Query q1 = ses.createQuery(query);
-
+		Query q1 = ses.createQuery("from Food where rate = :r");	// "Food" is the EntityName ; Also ByDefault EntityName is the Class name ; untill we specifically change it by doing  @Entity(name = "Food_Details") in Food class, in that case put "Food_Details"   
+		q1.setParameter("r", 50);									// But when we only change  @Table(name = "School_Details") ,  and no change in @Entity name, then in that case again our class name will be Entity Name, that we have to put here ClassName in HQL query 
+																	// And, after where condition for putting the where parameter -> either here we can write varible name "rate" or, Entity's Column changed name {@Column(name = "food_rate")} of Food class.
 		List<Food> foodList = q1.list();
 		for (Food f : foodList) {
 			System.out.println(f.getFid() + " -- " + f.getFoodName() + " -- " + f.getRate());
