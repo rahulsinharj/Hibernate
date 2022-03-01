c0b8378909556df273dc2df5aa3b2c87b5ef066f
rahul <rahul@192.168.1.3>
2022-03-01 11:29:32 +0530

    pagination

--- Hibernate1/src/main/java/com/Pagination/HQL_Pagination.java
+++ Hibernate1/src/main/java/com/Pagination/HQL_Pagination.java
@@ -20,8 +20,8 @@ public class HQL_Pagination {
  		-------------------------------------------------									*/
 		Query q = ses.createQuery("from Student");
 		
-		q.setFirstResult(7);		// firstly we can start with 0 
-		q.setMaxResults(20);
+		q.setFirstResult(7);		// firstly we can start with 0 , but currently we are starting with 8th element{7th index element , because indexing here also starts with 0} 
+		q.setMaxResults(20);		// Per page search kewal 20 entries hi dega. 	
 		List<Student> stuList = q.list();
 		for(Student s : stuList) {
 			System.out.println(s.getSid()+" "+s.getName()+" "+s.getCity());
